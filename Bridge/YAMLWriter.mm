69a1304fd9954b431338c6832844c3c3c652844c
Martin Kiss <martin.kiss@me.com>
2016-04-17 21:12:52 +0200

    [Writer]  Move Style setters to main implementation

--- Bridge/YAMLWriter.mm
+++ Bridge/YAMLWriter.mm
@@ -54,87 +54,16 @@
 
 @implementation YAMLWriter
 
+
 - (void)dealloc {
     delete self->_emitter;
 }
 
-@end
-
-
-#pragma mark - Initializers
-
-@implementation YAMLWriter (Initializers)
-
-- (nonnull instancetype)init {
-    self = [super init];
-    YAML_UNEXPECTED(self == nil);
-    
-    self->_emitter = new YAML::Emitter();
-    
-    return self;
-}
-
-- (nullable instancetype)initWithFileURL:(nonnull NSURL *)fileURL {
-    NSError *error = nil;
-    NSFileHandle *fileHandle = [NSFileHandle fileHandleForWritingToURL:fileURL error:&error];
-    
-    if (YAML_WARNING(fileHandle == nil, "Failed to create %@ for URL '%@': %@", NSFileHandle.class, fileURL, error.localizedDescription)) {
-        return nil;
-    }
-    
-    return [self initWithFileHandle:fileHandle];
-}
-
-- (nullable instancetype)initWithFileHandle:(nonnull NSFileHandle *)fileHandle {
-    self = [super init];
-    YAML_UNEXPECTED(self == nil);
-    
-    if (YAML_WARNING(fileHandle == nil)) {
-        return nil;
-    }
-    
-    int fileDescriptor = fileHandle.fileDescriptor;
-    YAMLFileDescriptorOutputStream stream(fileDescriptor);
-    self->_emitter = new YAML::Emitter(stream);
-    
-    return self;
-}
-
-@end
-
-
-#pragma mark - Result Accessors
-
-@implementation YAMLWriter (ResultAccessors)
-
-- (NSUInteger)outputLength {
-    return self->_emitter->size();
-}
-
-- (nonnull NSString *)outputString {
-    return @(self->_emitter->c_str());
-}
-
-- (nullable NSError *)error {
-    if (self->_emitter->good()) {
-        return nil;
-    }
-    
-    NSString *message = @(self->_emitter->GetLastError().c_str());
-    return [NSError errorWithDomain:YAMLErrorDomain
-                               code:YAMLErrorWriter
-                           userInfo:@{ NSLocalizedDescriptionKey: message }];
-}
-
-@end
-
 
 #pragma mark - Global Style
 
-@implementation YAMLWriter (GlobalStyle)
-
 // Since YAML::Emitter doesn’t expose these properties for reading, we store their copies.
-// If the underlaying emitter changes the properties different way, we may have irrelevant values.
+// If the underlaying emitter changes them in other way than these setters, we may have irrelevant values.
 
 - (void)setBooleanStyle:(YAMLStyleBoolean)style {
     self->_booleanStyle = style;
@@ -195,12 +124,86 @@
     self->_emitter->SetDoublePrecision([self validateDoublePrecision:precision]);
 }
 
+
+@end
+
+
+#pragma mark - Initializers
+
+@implementation YAMLWriter (Initializers)
+
+
+- (nonnull instancetype)init {
+    self = [super init];
+    YAML_UNEXPECTED(self == nil);
+    
+    self->_emitter = new YAML::Emitter();
+    
+    return self;
+}
+
+- (nullable instancetype)initWithFileURL:(nonnull NSURL *)fileURL {
+    NSError *error = nil;
+    NSFileHandle *fileHandle = [NSFileHandle fileHandleForWritingToURL:fileURL error:&error];
+    
+    if (YAML_WARNING(fileHandle == nil, "Failed to create %@ for URL '%@': %@", NSFileHandle.class, fileURL, error.localizedDescription)) {
+        return nil;
+    }
+    
+    return [self initWithFileHandle:fileHandle];
+}
+
+- (nullable instancetype)initWithFileHandle:(nonnull NSFileHandle *)fileHandle {
+    self = [super init];
+    YAML_UNEXPECTED(self == nil);
+    
+    if (YAML_WARNING(fileHandle == nil)) {
+        return nil;
+    }
+    
+    int fileDescriptor = fileHandle.fileDescriptor;
+    YAMLFileDescriptorOutputStream stream(fileDescriptor);
+    self->_emitter = new YAML::Emitter(stream);
+    
+    return self;
+}
+
+
+@end
+
+
+#pragma mark - Result Accessors
+
+@implementation YAMLWriter (ResultAccessors)
+
+
+- (NSUInteger)outputLength {
+    return self->_emitter->size();
+}
+
+- (nonnull NSString *)outputString {
+    return @(self->_emitter->c_str());
+}
+
+- (nullable NSError *)error {
+    if (self->_emitter->good()) {
+        return nil;
+    }
+    
+    NSString *message = @(self->_emitter->GetLastError().c_str());
+    return [NSError errorWithDomain:YAMLErrorDomain
+                               code:YAMLErrorWriter
+                           userInfo:@{ NSLocalizedDescriptionKey: message }];
+}
+
+
 @end
 
 
 #pragma mark - Style Utilities
 
 @implementation YAMLWriter (StyleUtilities)
+
 
 - (YAML::EMITTER_MANIP)booleanFormatManipulatorForStyle:(YAMLStyleBoolean)style {
     switch (style) {
@@ -398,6 +401,7 @@
     }
     return (int)precision;
 }
+
 
 @end
 
