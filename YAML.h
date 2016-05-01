0fa25545f5d6d9a810b16614d554e0720a177e5a
Martin Kiss <martin.kiss@me.com>
2016-05-01 18:39:29 +0200

    [New Core]  Create framework target with Swift, all builds

---
+++ YAML.h
@@ -0,0 +1,12 @@
+//
+//  YAML.h
+//  YAML.framework
+//
+//  Created by Martin Kiss on 1 May 2016.
+//  https://github.com/Tricertops/YAML
+//
+//  The MIT License (MIT)
+//  Copyright © 2016 Martin Kiss
+//
+
+#import <yaml.h>
